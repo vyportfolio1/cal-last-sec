3fb13d808b04c1dff7d573d43a0142615ac465a8
vyportfolio1 <vyportfolio1@gmail.com>
2023-12-30 10:34:54 +0200

    cal

--- index.js
+++ index.js
@@ -20,3 +20,30 @@ function clearInput() {
     result = 0;   
     document.getElementById('rezultat').value = '';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const onOffButton = document.querySelector('.on-off-button');
+    const inputField = document.getElementById('rezultat');
+
+    // Event listener for the toggle switch
+    onOffButton.addEventListener('change', function() {
+        if (this.querySelector('input').checked) {
+            // If the calculator is ON
+            inputField.classList.add('isiton');
+            inputField.value = ''; // Clear input field
+            inputField.removeAttribute('disabled'); // Enable input
+        } else {
+            // If the calculator is OFF
+            inputField.classList.remove('isiton');
+            inputField.value = 'Calculator is off'; // Display message
+            inputField.setAttribute('disabled', true); // Disable input
+        }
+    });
+
+    // Prevent typing when input is disabled
+    inputField.addEventListener('keydown', function(event) {
+        if (inputField.hasAttribute('disabled')) {
+            event.preventDefault(); // Prevent typing when disabled
+        }
+    });
+});
